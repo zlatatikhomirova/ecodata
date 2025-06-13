e50dff925b10280e9821febb4146c1a44352d6ab
zlata <algoneuro1@gmail.com>
2025-06-13 18:02:24 +0700

    created models only with types

--- system_design/db_design/sqla_models/address_rel_models.py
+++ system_design/db_design/sqla_models/address_rel_models.py
@@ -2,48 +2,84 @@ from datetime import datetime, date
 import re
 from uuid import uuid4
 
-from sqlalchemy import UUID, Column, DateTime, ForeignKey, MetaData, Numeric, String, Table, Date, Integer
+from sqlalchemy import (
+    UUID,
+    Column,
+    DateTime,
+    ForeignKey,
+    MetaData,
+    Numeric,
+    String,
+    Table,
+    Date,
+    Integer,
+)
 from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
 
 
 from .base_model import BaseSqlModel
 
+
 class HouseNumber(BaseSqlModel):
-    id: ...
-    number: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    number: Mapped[str] = mapped_column(String)
+
 
 class Street(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
 
 class Country(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
 
 class Region(BaseSqlModel):
-    id: ...
-    name: ...
-    country_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    country_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(Country.id), primary_key=True
+    )
+
 
 class District(BaseSqlModel):
-    id: ...
-    name: ...
-    region_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    region_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(Region.id), primary_key=True
+    )
+
 
 class SettlementType(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
 
 class SettlementName(BaseSqlModel):
-    id: ...
-    name: ...
-    district_id: ...
-    settlement_type_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    district_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(District.id), primary_key=True
+    )
+    settlement_type_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(SettlementType.id), primary_key=True
+    )
+
 
 class StreetSettlementAssociation(BaseSqlModel):
-    street_id: ...
-    settlement_name_id: ...
+    street_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(Street.id), primary_key=True
+    )
+    settlement_name_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(SettlementName.id), primary_key=True
+    )
+
 
 class Address(BaseSqlModel):
-    house_number_id: ...
-    street_settlement_association_id: ...
+    house_number_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(HouseNumber.id), primary_key=True
+    )
+    street_settlement_association_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(StreetSettlementAssociation.id), primary_key=True
+    )
--- system_design/db_design/sqla_models/models.py
+++ system_design/db_design/sqla_models/models.py
@@ -1,33 +1,83 @@
 from datetime import datetime, date
 
-from uuid import uuid4
+from uuid import UUID as PyUUID, uuid4
 
-from sqlalchemy import UUID, Column, DateTime, ForeignKey, MetaData, Numeric, String, Table, Date, Integer
+from sqlalchemy.dialects.postgresql import UUID
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    MetaData,
+    Numeric,
+    String,
+    Table,
+    Date,
+    Integer,
+    types,
+    func,
+)
 from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
 
-from .base_model import BaseSqlModel
+from .base import BaseSqlModel, NameCategory, created_at_utc
 from .address_rel_models import Address
 
+from typing import TYPE_CHECKING
+
+
 class JobTitle(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
+    # relationships
+    organization_details_list: Mapped[list["Organization"]] = relationship(
+        back_populates="job_title",
+    )
+
 
 class OrganizationDetails(BaseSqlModel):
-    id: ...
-    name: ...
-    email: ...
-    phone: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String)
+    phone: Mapped[str] = mapped_column(String)
+
+    # relationships
+    job_titles: Mapped[list["Organization"]] = relationship(
+        back_populates="organization_details",
+    )
+
 
 class Organization(BaseSqlModel):
-    address_id: ...
-    oranization_details_id: ...
+    id: Mapped[PyUUID] = mapped_column(
+        UUID, primary_key=True, default=uuid4, server_default=func.gen_random_uuid()
+    )
+    address_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(Address.id), primary_key=True
+    )
+    organization_details_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(OrganizationDetails.id), primary_key=True
+    )
+
+    # relationships
+    job_title: Mapped["JobTitle"] = relationship(
+        back_populates="organization_details_list"
+    )
+    organization_details: Mapped["OrganizationDetails"] = relationship(
+        back_populates="job_titles"
+    )
+
 
 class Job(BaseSqlModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    job_title_id: ...
+    job_title_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(JobTitle.id), primary_key=True
+    )
+
 
 class User(BaseSqlModel):
-    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    id: Mapped[PyUUID] = mapped_column(
+        UUID, primary_key=True, default=uuid4, server_default=func.gen_random_uuid()
+    )
     name: Mapped[str] = mapped_column(String)
     surname: Mapped[str] = mapped_column(String)
     patronymic: Mapped[str] = mapped_column(String)
@@ -40,223 +90,152 @@ class User(BaseSqlModel):
     username: Mapped[str] = mapped_column(String, unique=True)
     password_hash: Mapped[str] = mapped_column(String)
 
+
 class Role(BaseSqlModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, unique=True)
 
+
 class Status(BaseSqlModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: ...
+    name: Mapped[str] = mapped_column(String, unique=True)
+
 
 class Research(BaseSqlModel):
-    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
-    title: ...
-    goal: ...
-    description: ...
+    id: Mapped[PyUUID] = mapped_column(
+        UUID, primary_key=True, default=uuid4, server_default=func.gen_random_uuid()
+    )
+    title: Mapped[str] = mapped_column(String, unique=True)
+    goal: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
     dates: ...
-    status_id: ...
+    status_id: Mapped[int] = mapped_column(Integer, ForeignKey(Status.id))
+
 
 class UserResearchAssociation(BaseSqlModel):
-    research_id : ...
-    user_id: ...
+    research_id: Mapped[PyUUID] = mapped_column(
+        UUID, ForeignKey(Research.id), primary_key=True
+    )
+    user_id: Mapped[PyUUID] = mapped_column(UUID, ForeignKey(User.id), primary_key=True)
     role_id: Mapped[int] = mapped_column(Integer, ForeignKey(Role.id))
 
+
 class LeafType(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
 
 class Genus(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
 
-class Spesies(BaseSqlModel):
-    id: ...
-    name: ...
+
+class Species(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
+
+class LifeForm(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
 
 class PlantDescription(BaseSqlModel):
-    id: ...
-    life_form: ...
-    leaf_type_id: ...
-    genus_id: ...
-    species_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    life_form_id: Mapped[int] = mapped_column(Integer, ForeignKey(LifeForm.id))
+    leaf_type_id: Mapped[int] = mapped_column(Integer, ForeignKey(LeafType.id))
+    genus_id: Mapped[int] = mapped_column(Integer, ForeignKey(Genus.id))
+    species_id: Mapped[int] = mapped_column(Integer, ForeignKey(Species.id))
 
-class PollutionsNearPlace(BaseSqlModel):
-    address_id: ...
-    pollution_id: ...
 
 class Pollution(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
+
+class PollutionsNearPlace(BaseSqlModel):
+    address_id: Mapped[int] = mapped_column(Integer, ForeignKey(Address.id))
+    pollution_id: Mapped[int] = mapped_column(Integer, ForeignKey(Pollution.id))
 
 class Plant(BaseSqlModel):
-    address_id: ...
-    plant_description_id: ...
-
+    id: Mapped[PyUUID] = mapped_column(
+        UUID, primary_key=True, default=uuid4, server_default=func.gen_random_uuid()
+    )
+    address_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(Address.id)
+    )
+    plant_description_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey(PlantDescription.id)
+    )
 
 class ResearchPlantAssociation(BaseSqlModel):
-    research_id: ...
-    plant_id: ...
-    s3_key_final_morphological_result: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    research_id: Mapped[PyUUID] = mapped_column(
+        UUID, ForeignKey(Research.id), primary_key=True
+    )
+    plant_id: Mapped[PyUUID] = mapped_column(
+        UUID, ForeignKey(Research.id), primary_key=True
+    )
+    s3_key_final_morphological_result: Mapped[str] = mapped_column(String, unique=True)
+
 
 class PhotoDir(BaseSqlModel):
-    id: ...
-    research_plant_association_id: ...
-    name: ...
-    s3_key_joined_result_csv: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    research_plant_association_id:  Mapped[int] = mapped_column(Integer, ForeignKey(ResearchPlantAssociation.id))
+    name: Mapped[str] = mapped_column(String, unique=True)
+    s3_key_joined_result_csv: Mapped[str] = mapped_column(String, unique=True)
+
 
 class LeavesTemplatePhoto(BaseSqlModel):
-    id: ...
-    photo_dir_id: ...
-    s3_key_template: ...
-    s3_key_result_csv: ...
-    file_size: ...
-    uploaded_at: ...
-
-class BiochemAnalysis(BaseSqlModel):
-    id: ...
-    date: ...
-    additional_info: ...
-    research_plant_association_id: ...
-    organization_id: ...
-    s3_key: ...
-
-
-class BiochemAnalysisFeatureAssociation(BaseSqlModel):
-    biochem_analysis_id: ...
-    biochem_feature_id: ...
-    value: ...
-
-class BiochemFeature(BaseSqlModel):
-    id: ...
-    name: ...
-    measurement_unit_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    photo_dir_id: Mapped[int] = mapped_column(Integer, ForeignKey(PhotoDir.id))
+    s3_key_template: Mapped[str] = mapped_column(String, unique=True)
+    s3_key_result_csv: Mapped[str] = mapped_column(String, unique=True)
+    uploaded_at: Mapped[created_at_utc] # type: ignore
 
 class MeasurementUnit(BaseSqlModel):
-    id: ...
-    name: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
+class BiochemAnalysis(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    date: Mapped[date] = mapped_column(Date) # type: ignore
+    additional_info: Mapped[str] = mapped_column(String)
+    research_plant_association_id: Mapped[int] = mapped_column(Integer, ForeignKey(ResearchPlantAssociation.id))
+    organization_id: Mapped[int] = mapped_column(Integer, ForeignKey(Organization.id))
+    s3_key: Mapped[str] = mapped_column(String, unique=True)
+
+class BiochemFeature(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    measurement_unit_id: Mapped[int] = mapped_column(Integer, ForeignKey(MeasurementUnit.id))
+
+class BiochemAnalysisFeatureAssociation(BaseSqlModel):
+    biochem_analysis_id: Mapped[int] = mapped_column(Integer, ForeignKey(BiochemAnalysis.id))
+    biochem_feature_id: Mapped[int] = mapped_column(Integer, ForeignKey(BiochemFeature.id))
+    value: Mapped[int] = mapped_column(Integer)
 
 class MorphologicalFeature(BaseSqlModel):
-    id: ...
-    name: ...
-    measurement_unit_id: ...
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    measurement_unit_id: Mapped[int] = mapped_column(Integer, ForeignKey(MeasurementUnit.id))
+
+class LocationOnPlant(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
+class SideOfTheWorld(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+
+class Leaf(BaseSqlModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    side_of_the_world_id: Mapped[int] = mapped_column(Integer, ForeignKey(SideOfTheWorld.id), primary_key=True)
+    location_on_plant_id: Mapped[int] = mapped_column(Integer, ForeignKey(LocationOnPlant.id), primary_key=True)
+    s3_key_leaf_info: Mapped[str] = mapped_column(String, unique=True)
+    s3_key_leaf_mask: Mapped[str] = mapped_column(String, unique=True)
 
 class MorphologicalFeatureLeafAssociation(BaseSqlModel):
-    morphological_feature_id: ...
-    leaf_id: ...
-    value: ...
-
-class Leaf(BaseSqlModel):
-    id: ...
-    side_of_the_world: ...
-    location_on_plant: ...
-    s3_key_leaf_info: ...
-    s3_key_leaf_mask: ...
-
-
-
-
-
-
-
-    
-
-
-class Place(BaseSqlModel):
-    country: Mapped[str] = mapped_column(String)
-    region: Mapped[str] = mapped_column(String)
-    city: Mapped[str] = mapped_column(String)
-    district: Mapped[str] = mapped_column(String)
-    type_of_settlement: Mapped[str] = mapped_column(String)
-    
-    plants: Mapped[list["Plant"]] = relationship(back_populates="place")
-
-
-
-
-
-class Plant(BaseSqlModel):
-    form: Mapped[str] = mapped_column(String)
-    sheet_type: Mapped[str] = mapped_column(String)
-    family: Mapped[str] = mapped_column(String)
-    type: Mapped[str] = mapped_column(String)
-    place_id = mapped_column(ForeignKey(Place.id))
-    
-    place: Mapped[Place] = relationship(back_populates="plants")
-    researches: Mapped[list["Research"]] = relationship(back_populates="plant")
-
-
-class Research(BaseSqlModel):
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    target: Mapped[str] = mapped_column(String, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    start_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    end_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    status: Mapped[str] = mapped_column(String)
-    plant_id = mapped_column(ForeignKey(Plant.id))
-    
-    plant: Mapped[Plant] = relationship(back_populates="researches")
-
-
-class Article(BaseSqlModel):
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    plant_id = mapped_column(ForeignKey(Plant.id))
-    journal_name: Mapped[str] = mapped_column(String)
-    link: Mapped[str] = mapped_column(String)
-    file: Mapped[str] = mapped_column(String)
-
-
-class Laboratory(BaseSqlModel):
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    address: Mapped[str] = mapped_column(String, nullable=False)
-    phone: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, nullable=False)
-    
-    
-class Leaf(BaseSqlModel):
-    plant_id = mapped_column(ForeignKey(Plant.id))
-    date_of_measurement: Mapped[datetime] = mapped_column(DateTime)
-    side_of_the_world: Mapped[str] = mapped_column(String)
-    unit: Mapped[str] = mapped_column(String, nullable=False)
-    photo: Mapped[str] = mapped_column(String, nullable=False)
-    location_on_the_plant: Mapped[str] = mapped_column(String, nullable=False)
-    
-    
-class BioChem(BaseSqlModel):
-    chlorophyll_a: Mapped[float] = mapped_column(Numeric)
-    chlorophyll_b: Mapped[float] = mapped_column(Numeric)
-    carotenoids: Mapped[float] = mapped_column(Numeric)
-    phenols: Mapped[float] = mapped_column(Numeric)
-    anthocyanins: Mapped[float] = mapped_column(Numeric)
-    peroxidase: Mapped[float] = mapped_column(Numeric)
-    vitamin_c: Mapped[float] = mapped_column(Numeric)
-    unit: Mapped[str] = mapped_column(String, nullable=False)
-    note: Mapped[str] = mapped_column(String, nullable=False)
-    analysis_date: Mapped[datetime] = mapped_column(DateTime)
-    laboratory_id = mapped_column(ForeignKey(Laboratory.id))
-    plant_id = mapped_column(ForeignKey(Plant.id))
-    
-    
-class MorphologicalFeature(BaseSqlModel):
-    area: Mapped[float] = mapped_column(Numeric)
-    length: Mapped[float] = mapped_column(Numeric)
-    left_second_vein_length: Mapped[float] = mapped_column(Numeric)
-    right_second_vein_length: Mapped[float] = mapped_column(Numeric)
-    left_btw_first_n_second_veins_ends_dist: Mapped[float] = mapped_column(Numeric)
-    right_btw_first_n_second_veins_ends_dist: Mapped[float] = mapped_column(Numeric)
-    left_btw_first_n_second_veins_begins_dist: Mapped[float] = mapped_column(Numeric)
-    right_btw_first_n_second_veins_begins_dist: Mapped[float] = mapped_column(Numeric)
-    left_btw_second_n_central_veins_angle: Mapped[float] = mapped_column(Numeric)
-    right_btw_second_n_central_veins_angle: Mapped[float] = mapped_column(Numeric)
-    left_halfs_width: Mapped[float] = mapped_column(Numeric)
-    right_halfs_width: Mapped[float] = mapped_column(Numeric)
-    leaf_id = mapped_column(ForeignKey(Leaf.id))
-
-    
-order_mtm_product_table = Table(
-    'specialist_in_research',
-    BaseSqlModel.metadata,
-    Column('research_id', ForeignKey('research.id')),
-    Column('specialist_id', ForeignKey('specialist.id')),
-)
-
-    
+    morphological_feature_id: Mapped[int] = mapped_column(Integer, ForeignKey(MorphologicalFeature.id), primary_key=True)
+    leaf_id: Mapped[int] = mapped_column(Integer, ForeignKey(Leaf.id), primary_key=True)
+    value: Mapped[int] = mapped_column(Integer)
