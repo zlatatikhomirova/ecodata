5e8dbbb1b9669e82f9c2447868aeb6d12f6d1480
zlata <algoneuro1@gmail.com>
2025-06-15 16:25:56 +0700

    leaves template photo rel models checked

--- system_design/db_design/sqla_models/leaves_template_photo_rel_models.py
+++ system_design/db_design/sqla_models/leaves_template_photo_rel_models.py
@@ -1,31 +1,15 @@
-from datetime import datetime, date
-
-from uuid import UUID as PyUUID, uuid4
-
-from sqlalchemy.dialects.postgresql import UUID
-
-from sqlalchemy import (
-    Column,
-    DateTime,
-    ForeignKey,
-    MetaData,
-    Numeric,
-    String,
-    Table,
-    Date,
-    Integer,
-    types,
-    func,
-)
-from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
-
-from .base import BaseSqlModel, NameCategory, created_at_utc
-from .address_rel_models import Address
-
 from typing import TYPE_CHECKING
 
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    Integer,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import BaseSqlModel, created_at_utc
+
 if TYPE_CHECKING:
-    from .organization_rel_models import Organization
     from .photo_dir_rel_models import PhotoDir
     from .leaf_rel_models import Leaf
 
